aca0b2e4b331abfe31040c01673f951029545bfa
Guo Jian Ng <NG@Guo-Jian-Ngs-MacBook-Pro.local>
2016-05-03 14:53:47 -0700

    updated

--- names.py
+++ names.py
@@ -1,1 +1,38 @@
-# Code coming soon!
+# context = {
+#   'questions': [
+#    { 'id': 1, 'content': 'Why is there a light in the fridge and not in the freezer?'},
+#    { 'id': 2, 'content': 'Why don\'t sheep shrink when it rains?'},
+#    { 'id': 3, 'content': 'Why are they called apartments when they are all stuck together?'},
+#    { 'id': 4, 'content': 'Why do cars drive on the parkway and park on the driveway?'}
+#   ]
+#  }
+#
+# for key, data in context.items():
+#      #print data
+#      for value in data:
+#           print "Question #", value["id"], ": ", value["content"]
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+for item in range(len(students)):
+    print students[item]["first_name"], students[item]["last_name"]
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+for key, value in users.items():
+    print key
+    for i in range(len(value)):
+        print i+1, '-', value[i]["first_name"], value[i]["last_name"],"-", len(value[i]["first_name"])+len(value[i]["last_name"])
